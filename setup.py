c230e3c630a1bc6a38258c0fc901eddb7439f18b
18832823200 <113670369+18832823200@users.noreply.github.com>
2022-09-19 08:57:59 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding: utf-8
+from setuptools import setup, find_packages
+setup(
+    name="pip_model",
+    version="0.1",
+    license="MIT Licence",
+    url="https://github.com/18832823200/pip_model",
+    author="sbdsyf",
+    author_email="zkw8119@163.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
